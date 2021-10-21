3dad05a1b83de45bc0fc7a0290416a7ba045b574
Aadarsh <aadarshraj56@gmail.com>
2021-10-21 20:47:29 +0530

    few updates

--- main.py
+++ main.py
@@ -9,33 +9,33 @@ screen = pygame.display.set_mode((1000,570))
 
 pygame.display.set_caption("Mario Game")
 
-icon = pygame.image.load("/Users/aadarsh/Documents/VS Code/Mario_Game/images/icon.png")
+icon = pygame.image.load("/Users/aadarsh/Documents/VSCode/Mario_Game/images/icon.png")
 pygame.display.set_icon(icon)
 icon = pygame.transform.scale(icon,(100,100))
 
-background = pygame.image.load("/Users/aadarsh/Documents/VS Code/Mario_Game/images/background.png")
+background = pygame.image.load("/Users/aadarsh/Documents/VSCode/Mario_Game/images/background.png")
 
-base = pygame.image.load("/Users/aadarsh/Documents/VS Code/Mario_Game/images/base.png")
+base = pygame.image.load("/Users/aadarsh/Documents/VSCode/Mario_Game/images/base.png")
 base = pygame.transform.scale(base,(1000,120))
 
-start = pygame.image.load("/Users/aadarsh/Documents/VS Code/Mario_Game/images/start1.png")
+start = pygame.image.load("/Users/aadarsh/Documents/VSCode/Mario_Game/images/start1.png")
 start = pygame.transform.scale(start,(300,300))
 
-pipe = pygame.image.load("/Users/aadarsh/Documents/VS Code/Mario_Game/images/pipe.png")
+pipe = pygame.image.load("/Users/aadarsh/Documents/VSCode/Mario_Game/images/pipe.png")
 pipe = pygame.transform.scale(pipe,(100,300))
 
 digits ={}
 digits['numbers'] = [ 
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/0.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/1.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/2.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/3.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/4.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/5.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/6.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/7.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/8.png').convert_alpha(),
-        pygame.image.load('/Users/aadarsh/Documents/VS Code/Mario_Game/images/9.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/0.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/1.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/2.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/3.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/4.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/5.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/6.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/7.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/8.png').convert_alpha(),
+        pygame.image.load('/Users/aadarsh/Documents/VSCode/Mario_Game/images/9.png').convert_alpha(),
     ]
 for i in range(10):
     digits['numbers'][i]=pygame.transform.scale(digits['numbers'][i],(64,64))
@@ -101,7 +101,7 @@ def Game():
     accelerate=9
     air=False
     score=0
-    pygame.mixer.Sound("/Users/aadarsh/Documents/VS Code/Mario_Game/sounds/background.mp3").play()
+    pygame.mixer.Sound("/Users/aadarsh/Documents/VSCode/Mario_Game/sounds/background.mp3").play()
     while True:
         font=pygame.font.Font(pygame.font.get_default_font(), 100)
         result = font.render("SCORE : " + str(score),True,(255,0,0))
@@ -137,17 +137,16 @@ def Game():
             
         if collide(playerx,playery-playerchangey,pipex1-pipechangex1,pipey1+pipechangey1):
             pygame.mixer.pause()
-            pygame.mixer.Sound("/Users/aadarsh/Documents/VS Code/Mario_Game/sounds/gameover.mp3").play()
+            pygame.mixer.Sound("/Users/aadarsh/Documents/VSCode/Mario_Game/sounds/gameover.mp3").play()
             break
         if collide(playerx,playery-playerchangey,pipex2-pipechangex2,pipey2+pipechangey2):
             pygame.mixer.pause()
-            pygame.mixer.Sound("/Users/aadarsh/Documents/VS Code/Mario_Game/sounds/gameover.mp3").play()
+            pygame.mixer.Sound("/Users/aadarsh/Documents/VSCode/Mario_Game/sounds/gameover.mp3").play()
             break
         if overlapp(playerx,playery-playerchangey,pipex1-pipechangex1,pipey1+pipechangey1) and accelerate<0:
             playerchangey-=accelerate
         if overlapp(playerx,playery-playerchangey,pipex2-pipechangex2,pipey2+pipechangey2) and accelerate<0:
             playerchangey-=accelerate
-        pygame.display.update()
         if check_score(playerx,playery-playerchangey,pipex1-pipechangex1,pipey1+pipechangey1):
             score+=1
         if check_score(playerx,playery-playerchangey,pipex2-pipechangex2,pipey2+pipechangey2):
